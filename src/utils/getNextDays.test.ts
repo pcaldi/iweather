a09baa08241b6dc63bf9bf3a6ba6f4b609435257
pcaldi <pcaldi@gmail.com>
2023-09-26 19:21:46 -0300

    test: test getNextDays

--- src/utils/getNextDays.test.ts
+++ src/utils/getNextDays.test.ts
@@ -1,10 +1,12 @@
 import { getNextDays } from "./getNextDays";
 
+describe("getNextDays", () => {
+  it('should be return the next five days', () => {
+    // Executar o teste...
 
-test('should be return the next five days', () => {
-  // Executar o teste...
+    const days = getNextDays();
 
-  const days = getNextDays();
+    expect(days.length).toBe(5);
+  });
 
-  expect(days.length).toBe(5);
-})
+});
