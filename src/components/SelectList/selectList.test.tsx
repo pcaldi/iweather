35c5e5006ce157336244987c6046734f04f8dc2a
pcaldi <pcaldi@gmail.com>
2023-09-27 16:16:52 -0300

    test:mock selectList

---
+++ src/components/SelectList/selectList.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react-native";
+
+import { SelectList } from "@components/SelectList";
+
+describe("Component: SelectList", () => {
+  it("should be ...", () => {
+
+
+  });
+
+});
