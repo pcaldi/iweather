80f7a7c67bdd0aa852ef1fc6b7a003e81b03671d
pcaldi <pcaldi@gmail.com>
2023-09-27 14:57:04 -0300

    test: finalizando test input

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -4,9 +4,19 @@ import { Input } from "@components/Input";
 
 describe("Component: Input", () => {
   it("should be render without activity indicator if isLoading prop is undefined", () => {
+    render(<Input />);
+
+    const activityIndicator = screen.queryByTestId("activity-indicator")
+    expect(activityIndicator).toBeNull();
+  });
+
+  it("should bee render without activity indicator if isLoading prop is true", () => {
     render(<Input isLoading />);
 
-    const activityIndicator = screen.getByTestId("activity-indicator")
-    console.log(activityIndicator);
+    const activityIndicator = screen.getByTestId("activity-indicator");
+
+    expect(activityIndicator).toBeTruthy();
+
   })
+
 });
