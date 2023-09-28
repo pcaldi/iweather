60dbc9dd9a3aa970a7f177ce939e63cddd2338a4
pcaldi <pcaldi@gmail.com>
2023-09-28 16:31:50 -0300

    test: removeStorageCity

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -1,19 +1,22 @@
-import { getStorageCity, saveStorageCity } from "@libs/asyncStorage/cityStorage";
+import { getStorageCity, removeStorageCity, saveStorageCity } from "@libs/asyncStorage/cityStorage";
 import { CityProps } from "@services/getCityByNameService";
+
+
+const newCity: CityProps = {
+  id: '1',
+  name: 'Curitiba',
+  latitude: 123,
+  longitude: 456
+}
 
 describe("Storage: cityStorage", () => {
   it("should be return null when don't a city storage", async () => {
     const response = await getStorageCity();
     expect(response).toBeNull();
-  })
+  });
 
   it("should be return city storage", async () => {
-    const newCity: CityProps = {
-      id: '1',
-      name: 'Curitiba',
-      latitude: 123,
-      longitude: 456
-    }
+
 
     await saveStorageCity(newCity);
     const response = await getStorageCity();
@@ -21,5 +24,14 @@ describe("Storage: cityStorage", () => {
     expect(response).toEqual(newCity);
 
 
-  })
+  });
+
+  it("should be remove city storage", async () => {
+    await saveStorageCity(newCity);
+    await removeStorageCity();
+
+    const response = await getStorageCity();
+
+    expect(response).toBeNull();
+  });
 })
