e04ae2305156d6117dddfc17b6992cc90718628b
pcaldi <pcaldi@gmail.com>
2023-09-30 11:17:17 -0300

    test: rotas

--- src/routes/routes.test.tsx
+++ src/routes/routes.test.tsx
@@ -1,5 +1,9 @@
-import { render, waitFor, screen } from "@testing-library/react-native"
+
 import { Routes } from "."
+import { api } from "@services/api";
+import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
+import { render, waitFor, screen, act } from "@__tests__/utils/customRender"
+import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherAPIResponse";
 
 
 describe("Routes", () => {
@@ -10,5 +14,26 @@ describe("Routes", () => {
 
     expect(title).toBeTruthy();
 
-  })
+  });
+
+  it("should be render dashboard screen when city selected.", async () => {
+
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockWeatherAPIResponse });
+
+    const city = {
+      id: '1',
+      name: 'Curitiba',
+      latitude: 123,
+      longitude: 321
+    };
+
+    await saveStorageCity(city);
+
+    await act(() => waitFor(() => render(<Routes />)));
+
+    const title = screen.getByText(city.name);
+    expect(title).toBeTruthy();
+
+
+  });
 })
