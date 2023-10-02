c6f85a7483e05c0cd680dbc17fddcbfed2847bef
pcaldi <pcaldi@gmail.com>
2023-10-02 20:24:34 -0300

    test: screen search

---
+++ src/screens/Search/search.test.tsx
@@ -0,0 +1,19 @@
+import { Search } from ".";
+import { api } from "@services/api"
+import { mockCityAPIResponse } from "@__tests__/mocks/api/mockCityAPIResponse"
+import { render, screen, fireEvent, waitFor } from "@testing-library/react-native";
+
+describe('Screen: Search', () => {
+  it('should be show options city.', async () => {
+
+    jest.spyOn(api, 'get').mockResolvedValue({ data: mockCityAPIResponse });
+
+    render(<Search />);
+
+    const searchInput = screen.getByTestId('search-input');
+    fireEvent.changeText(searchInput, 'Curitiba');
+
+    const option = await waitFor(() => screen.findByText(/curitiba/i));
+    expect(option).toBeTruthy();
+  })
+})
