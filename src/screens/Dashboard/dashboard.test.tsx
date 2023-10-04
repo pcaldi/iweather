89fe16d6075c8430cf924edec57b236c02a63f2e
pcaldi <pcaldi@gmail.com>
2023-10-04 16:36:19 -0300

    test: screen Dashboard

--- src/screens/Dashboard/dashboard.test.tsx
+++ src/screens/Dashboard/dashboard.test.tsx
@@ -1,13 +1,13 @@
 import { Dashboard } from "@screens/Dashboard";
 import { api } from "@services/api"
 import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
-import { render, screen, waitFor } from "@__tests__/utils/customRender";
+import { act, fireEvent, render, screen, waitFor, waitForElementToBeRemoved } from "@__tests__/utils/customRender";
 import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherAPIResponse"
+import { mockCityAPIResponse } from "@__tests__/mocks/api/mockCityAPIResponse";
 
 describe("Screen: Dashboard", () => {
-  it("should be show city weather.", async () => {
-    jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse });
 
+  beforeAll(async () => {
     const city = {
       id: '1',
       name: 'Matinhos, BR',
@@ -15,7 +15,11 @@ describe("Screen: Dashboard", () => {
       longitude: 456
     }
 
-    await saveStorageCity(city)
+    await saveStorageCity(city);
+  })
+
+  it("should be show city weather.", async () => {
+    jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse });
 
     render(<Dashboard />)
 
@@ -23,6 +27,45 @@ describe("Screen: Dashboard", () => {
     const cityName = await waitFor(() => screen.findByText(/matinhos/i));
     expect(cityName).toBeTruthy()
 
-  })
+  });
+
+  it("should be show another selected weather city.", async () => {
+
+    /*
+    1 - Buscar informações do clima/tempo da cidade selecionada.
+    2 - Buscar informações da cidade.
+    3 - Buscar informações do clima/tempo da nova cidade selecionada.
+    */
+
+    jest.spyOn(api, 'get')
+      .mockResolvedValueOnce({ data: mockWeatherAPIResponse })
+      .mockResolvedValueOnce({ data: mockCityAPIResponse })
+      .mockResolvedValueOnce({ data: mockWeatherAPIResponse });
+
+
+    render(<Dashboard />)
+
+    await waitForElementToBeRemoved(() => screen.queryByTestId("loading"));
+
+
+    const cityName = 'Curitiba'
+
+    await waitFor(() => act(() => {
+      const search = screen.getByTestId("search-input")
+      fireEvent.changeText(search, cityName)
+    }))
+
+    await waitFor(() => act(() => {
+      fireEvent.press(screen.getByText(cityName, { exact: false }))
+    }));
+
+    expect(screen.getByText(cityName, { exact: false })).toBeTruthy();
+
+
+  });
+
+
+
+
 })
 
